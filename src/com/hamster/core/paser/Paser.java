9b82ea7b3cb497541e05956a79ce50d8f282ceb2
Administrator <Administrator@UWN9HWB4FMWAUHF>
2018-02-02 23:13:33 +0800

    20180202

---
+++ src/com/hamster/core/paser/Paser.java
@@ -0,0 +1,11 @@
+package com.hamster.core.paser;
+
+public class Paser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Start Analyse...");
+	}
+
+}
